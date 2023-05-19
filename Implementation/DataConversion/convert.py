6e23a144d6b6ebb984305a18a4922b11f80f8056
David Kreisler <a01569177@unet.univie.ac.at>
2023-05-19 13:05:09 +0200

    converter adaption

--- Implementation/DataConversion/convert.py
+++ Implementation/DataConversion/convert.py
@@ -5,38 +5,3 @@ def convertToPandas(csv_name: str, separator: str = ',') -> pd.DataFrame:
     df.replace(to_replace=' ', value=0, inplace=True)
     return df
 
-def convertToDict(csv_name: str, separator: str = ',') -> dict:
-
-    df = pd.read_csv(csv_name, sep=separator)
-
-    df = df.reset_index()  # make sure indexes pair with number of rows
-
-    keys = df['object'].unique()
-    data = dict()
-
-    for key in keys:
-        data[key] = dict()
-
-    for index, row in df.iterrows():
-        try:
-            _, _, objectId = row['drug1'].split(':')
-        except AttributeError:
-            objectId = row['drug1']
-        except ValueError:
-            objectId = row['drug1']
-
-        try:
-            _, _, interactionId = row['drug2'].split(':')
-        except AttributeError:
-            interactionId = row['drug2']
-        except ValueError:
-            interactionId = row['drug1']
-
-        data[row['object']]['Id'] = objectId
-
-        data[row['object']][row['precipitant']] = dict()
-        data[row['object']][row['precipitant']]['Id'] = interactionId
-        data[row['object']][row['precipitant']]['severity'] = row['severity']
-
-    return data
-
